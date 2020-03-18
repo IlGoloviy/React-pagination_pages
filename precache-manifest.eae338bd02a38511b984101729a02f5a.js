57cdfc8db97e718387ab17ce8c5744425199eda3
Illia Holovii <goloviyillya@gmail.com>
2020-03-18 17:37:58 +0200

    Updates

---
+++ precache-manifest.eae338bd02a38511b984101729a02f5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd181103e5f7363f5e4ffa8b424283b2",
+    "url": "/React-pagination_pages/index.html"
+  },
+  {
+    "revision": "d339eac58a6723bc9fbf",
+    "url": "/React-pagination_pages/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d07d8533fca9e855adb9",
+    "url": "/React-pagination_pages/static/js/2.ae2d48f9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/React-pagination_pages/static/js/2.ae2d48f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d339eac58a6723bc9fbf",
+    "url": "/React-pagination_pages/static/js/main.5d5fe3e6.chunk.js"
+  },
+  {
+    "revision": "39e89672cdf59498d58a",
+    "url": "/React-pagination_pages/static/js/runtime-main.e641706b.js"
+  }
+]);
